f85eb59ae46520b2c3cf4ead05095f49aa65afcd
OziOcb <pawel_ozi@o2.pl>
2020-03-13 13:43:06 +0000

    Remove the SectionContact.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,22 +2,19 @@
   <layout>
     <main role="main">
       <SectionHero />
-      <SectionContact />
     </main>
   </layout>
 </template>
 
 <script>
 import SectionHero from "@/components/SectionHero.vue"
-import SectionContact from "@/components/SectionContact.vue"
 
 export default {
   metaInfo: {
     title: "Home Page"
   },
   components: {
-    SectionHero,
-    SectionContact
+    SectionHero
   }
 }
 </script>
