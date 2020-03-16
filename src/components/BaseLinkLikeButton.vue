9bff134c9a7951d20044c12bf7be691e19f0b0bd
OziOcb <pawel_ozi@o2.pl>
2020-03-16 14:36:27 +0000

    Fix global variable names

--- src/components/BaseLinkLikeButton.vue
+++ src/components/BaseLinkLikeButton.vue
@@ -32,28 +32,28 @@ export default {
 <style lang="scss" scoped>
 .baseLinkLikeButton {
   @extend %typography-button-alt;
-  padding: $size-button-alt-padding;
+  padding: $size-buttonAlt-padding;
   display: inline-block;
   font-weight: bold;
   text-decoration: none;
-  color: $color-button-alt-text;
-  background: $color-button-alt-bg;
-  border: 2px solid $color-button-alt-border;
+  color: $color-buttonAlt-text;
+  background: $color-buttonAlt-bg;
+  border: 2px solid $color-buttonAlt-border;
   border-radius: 0.625rem;
-  box-shadow: 0px 0.6em 1.25em $color-button-alt-shadow;
+  box-shadow: 0px 0.6em 1.25em $color-buttonAlt-shadow;
   transition: background $duration-animation-base linear, box-shadow $duration-animation-base linear,
     transform $duration-animation-base linear;
 
   &:hover,
   &:focus {
-    background: $color-button-alt-bg-hover;
-    box-shadow: 0px 1.25em 2.5em $color-button-alt-shadow;
+    background: $color-buttonAlt-bg-hover;
+    box-shadow: 0px 1.25em 2.5em $color-buttonAlt-shadow;
     transform: scale(1.03);
   }
 
   &:focus {
     outline: 0;
-    box-shadow: 0px 1.25em 2.5em $color-button-alt-shadow, 0 0 2px 4px $color-outline;
+    box-shadow: 0px 1.25em 2.5em $color-buttonAlt-shadow, 0 0 2px 4px $color-outline;
   }
 }
 </style>
