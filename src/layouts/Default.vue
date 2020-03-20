992742a7af64d1896f6f4fa2449bbd1d3f78c9d7
OziOcb <pawel_ozi@o2.pl>
2020-03-20 16:40:30 +0000

    Add a subtle page transition

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,9 +2,12 @@
   <div class="layout">
     <TheHeader />
 
-    <slot />
-
-    <TheFooter />
+    <transition :css="false" appear @enter="enter">
+      <div>
+        <slot />
+        <TheFooter />
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -12,10 +15,30 @@
 import TheHeader from "@/components/TheHeader.vue"
 import TheFooter from "@/components/TheFooter.vue"
 
+import { gsap, Power4 } from "gsap"
+
 export default {
   components: {
     TheHeader,
     TheFooter
+  },
+  methods: {
+    enter(el, done) {
+      gsap.fromTo(
+        el,
+        0.6,
+        {
+          autoAlpha: 0,
+          y: 20
+        },
+        {
+          autoAlpha: 1,
+          y: 0,
+          ease: Power4.ease,
+          onComplete: done
+        }
+      )
+    }
   }
 }
 </script>
