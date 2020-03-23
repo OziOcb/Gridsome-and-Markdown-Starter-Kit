a6a8c5d12c8040de5ad760842622049b280feb99
OziOcb <pawel_ozi@o2.pl>
2020-03-23 00:27:03 +0000

    Add src/utils/breakpoints.js file for global breakpoint sizes

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,10 @@
+// If you change anything in here you must change the same thing inside:
+// src/assets/sass/globals/_breakpoints.scss
+export default {
+  xs: 320, // smartphones, portrait iPhone, portrait 480x320 phones (Android)
+  sm: 480, // smartphones, Android phones, landscape iPhone
+  md: 600, // portrait tablets, portrait iPad, e-readers (Nook/Kindle), landscape 800x480 phones (Android),
+  lg: 801, //tablet, landscape iPad, lo-res laptops ands desktops
+  xl: 1025, //big landscape tablets, laptops, and desktops
+  xxl: 1281 //hi-res laptops and desktops
+}
